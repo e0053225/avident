f5c59045dadafd2677f8c5404446f6e92332ded1
tanlongbin <e0031490@u.nus.edu>
2019-10-09 21:17:04 +0800

    Implement task list and styling for dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -22,92 +22,109 @@ import {
   IonChip,
   IonLabel,
   IonBadge,
-  IonIcon
+  IonIcon,
+  IonList,
+  IonItem,
+  IonSegment,
+  IonSegmentButton
 } from "@ionic/react";
-import { notifications } from 'ionicons/icons';
+import {
+  notifications,
+  book,
+  cart,
+  construct,
+  qrScanner
+} from "ionicons/icons";
 import "./Dashboard.css";
+import { TaskList } from "./taskList";
 
 const DashboardComponent = ({ history }: { [key: string]: any }) => (
   <IonPage>
-          <IonToolbar>
-              <IonChip slot="end">
-                  <IonIcon icon={notifications}/>
-              <IonBadge color="warning">3</IonBadge>
-              </IonChip>
+    <IonToolbar>
+      <IonChip slot="end">
+        <IonIcon icon={notifications} />
+        <IonBadge color="warning">3</IonBadge>
+      </IonChip>
 
-              <IonChip slot="end">
+      <IonChip slot="end">
         <IonLabel>Randy Tan</IonLabel>
-      <IonAvatar>
-      <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
-      </IonAvatar>
+        <IonAvatar>
+          <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
+        </IonAvatar>
       </IonChip>
     </IonToolbar>
-    <IonContent>
+    <IonContent scrollY={false}>
       <IonGrid>
         <IonRow class="top-row">
-          <IonCol>
-            <IonCard color="light">
-              <IonCardTitle>Welcome back, Randy!</IonCardTitle>
-              <IonCardContent>1</IonCardContent>
+          <IonCol size="4.5">
+            <div className="welcome-text-box">
+            <IonText class="welcome-text">Welcome back, Randy!</IonText>
+            </div>
+          </IonCol>
+          <IonCol size="2.5">
+            <IonCard class="header-card" color="light">
+              <IonCardTitle class="header-card-title">18/10/2019</IonCardTitle>
+              <IonCardContent class="header-card-content">5</IonCardContent>
+              <IonCardContent class="header-card-footer">Scheduled Task Remaining</IonCardContent>
             </IonCard>
           </IonCol>
-          <IonCol>
-            <IonCard color="light">
-              <IonCardTitle>Date</IonCardTitle>
-              <IonCardContent>1</IonCardContent>
-              <IonCardSubtitle>Scheduled Task Remaining</IonCardSubtitle>
+          <IonCol size="2.5">
+          <IonCard class="header-card" color="light">
+              <IonCardTitle class="header-card-title">Task Completed</IonCardTitle>
+              <IonCardContent class="header-card-content">56</IonCardContent>
+              <IonCardContent class="header-card-footer">You are level 17</IonCardContent>
             </IonCard>
           </IonCol>
-          <IonCol>
-            <IonCard color="light">
-              <IonCardTitle>Task Completed</IonCardTitle>
-              <IonCardContent>1</IonCardContent>
-              <IonCardSubtitle>You are level 17</IonCardSubtitle>
-            </IonCard>
-          </IonCol>
-          <IonCol>
-            <IonCard color="light">
-              <IonCardTitle>Total Points</IonCardTitle>
-              <IonCardContent>1</IonCardContent>
-              <IonCardSubtitle>Redeem Points</IonCardSubtitle>
+          <IonCol size="2.5">
+          <IonCard class="header-card" color="light">
+              <IonCardTitle class="header-card-title">Total Points</IonCardTitle>
+              <IonCardContent class="header-card-content">379 pts</IonCardContent>
+              <IonCardContent class="header-card-footer">Redeem Points</IonCardContent>
             </IonCard>
           </IonCol>
         </IonRow>
         <IonRow>
-          <IonCol>
-            <IonCard color="light">
-              <IonCardTitle class="header">
-                <IonText color="light">Your Task</IonText>
-              </IonCardTitle>
-              <IonCardContent>1</IonCardContent>
-            </IonCard>
+          <IonCol size="7">
+            <TaskList></TaskList>
           </IonCol>
           <IonCol>
             <IonRow>
-              <IonCol>
-                <IonCard color="light">
-                  <IonCardHeader>Tech Manual</IonCardHeader>
-                  <IonCardContent>1</IonCardContent>
+              <IonCol size="6">
+                <IonCard button color="light">
+                  <IonCardHeader class="card-title">Tech Manual</IonCardHeader>
+                  <IonCardContent class="card-icon">
+                    <IonIcon class="icon-size" icon={book} />
+                  </IonCardContent>
                 </IonCard>
               </IonCol>
-              <IonCol>
-                <IonCard color="light">
-                  <IonCardHeader>Order Parts</IonCardHeader>
-                  <IonCardContent>2</IonCardContent>
+              <IonCol size="6">
+                <IonCard button color="light">
+                  <IonCardHeader class="card-title">Order Parts</IonCardHeader>
+                  <IonCardContent class="card-icon">
+                    <IonIcon class="icon-size" icon={cart} />
+                  </IonCardContent>
                 </IonCard>
               </IonCol>
             </IonRow>
             <IonRow>
-              <IonCol>
-                <IonCard color="light">
-                  <IonCardHeader>Report Defects</IonCardHeader>
-                  <IonCardContent>3</IonCardContent>
+              <IonCol size="6">
+                <IonCard button color="light">
+                  <IonCardHeader class="card-title">
+                    Report Defects
+                  </IonCardHeader>
+                  <IonCardContent class="card-icon">
+                    <IonIcon class="icon-size" icon={construct} />
+                  </IonCardContent>
                 </IonCard>
               </IonCol>
-              <IonCol>
-                <IonCard color="light">
-                  <IonCardHeader>Identify Parts</IonCardHeader>
-                  <IonCardContent>4</IonCardContent>
+              <IonCol size="6">
+                <IonCard button color="light">
+                  <IonCardHeader class="card-title">
+                    Identify Parts
+                  </IonCardHeader>
+                  <IonCardContent class="card-icon">
+                    <IonIcon class="icon-size" icon={qrScanner} />
+                  </IonCardContent>
                 </IonCard>
               </IonCol>
             </IonRow>
---
+++ src/pages/Dashboard/taskList.tsx
@@ -0,0 +1,167 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { withAuthorization } from "../../firebase/withAuthorization";
+import {
+  IonPage,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonCol,
+  IonRow,
+  IonInput,
+  IonText,
+  IonGrid,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonAvatar,
+  IonChip,
+  IonLabel,
+  IonBadge,
+  IonIcon,
+  IonList,
+  IonItem,
+  IonSegmentButton,
+  IonSegment,
+  IonImg,
+  IonProgressBar
+} from "@ionic/react";
+import { closeCircle } from "ionicons/icons";
+import planeImg from "../../assets/imgs/SIA-B747.png";
+
+class TaskListComponent extends Component {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      list: [],
+      selectedStatus: ""
+    };
+  }
+  public componentDidMount() {}
+
+  public render() {
+    return (
+      <IonCard color="light">
+        <IonCardTitle class="header">
+          <IonText color="light">Your Tasks</IonText>
+        </IonCardTitle>
+        <IonToolbar>
+          <div slot="end">
+            <IonSegment>
+              <IonSegmentButton
+                onIonSelect={() => this.setState({ selectedStatus: "New" })}
+                class="status-button"
+              >
+                <IonLabel>New</IonLabel>
+              </IonSegmentButton>
+              <IonSegmentButton
+                onIonSelect={() => this.setState({ selectedStatus: "Started" })}
+                class="status-button"
+              >
+                <IonLabel>Started</IonLabel>
+              </IonSegmentButton>
+              <IonSegmentButton
+                onIonSelect={() =>
+                  this.setState({ selectedStatus: "Completed" })
+                }
+                class="status-button"
+              >
+                <IonLabel>Completed</IonLabel>
+              </IonSegmentButton>
+            </IonSegment>
+          </div>
+        </IonToolbar>
+        <IonCardContent class="task-list">
+          <IonContent>
+            <IonList>
+              <IonCard button color="light">
+                <IonCardHeader class="ion-no-padding">
+                  <h1>TASK 32-11-02-004-001</h1>
+                </IonCardHeader>
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonText>
+                        <h2>Wing Landing Gear Shock Struct Removal</h2>
+                      </IonText>
+                    </IonCol>
+                    <IonCol>
+                      <IonCardContent class="ion-no-padding">
+                        <IonImg src={planeImg} />
+                        <IonText>9V-SPK BOEING-747-412</IonText>
+                      </IonCardContent>
+                    </IonCol>
+                  </IonRow>
+                      <IonText>Progress:</IonText>
+                      <IonProgressBar
+                        color="warning"
+                        value={0.9}
+                      ></IonProgressBar>
+                </IonGrid>
+              </IonCard>
+              <IonCard button color="light">
+                <IonCardHeader class="ion-no-padding">
+                  <h1>TASK 32-11-02-404-019</h1>
+                </IonCardHeader>
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonText>
+                        <h2>Wing Landing Gear Shock Struct Installation</h2>
+                      </IonText>
+                    </IonCol>
+                    <IonCol>
+                      <IonCardContent class="ion-no-padding">
+                        <IonImg src={planeImg} />
+                        <IonText>9V-SPK BOEING-747-412</IonText>
+                      </IonCardContent>
+                    </IonCol>
+                  </IonRow>
+                      <IonText>Progress:</IonText>
+                      <IonProgressBar
+                        color="error"
+                        value={0}
+                      ></IonProgressBar>
+                </IonGrid>
+              </IonCard>
+              <IonCard button color="light">
+                <IonCardHeader class="ion-no-padding">
+                  <h1>TASK 32-11-02-603-001</h1>
+                </IonCardHeader>
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonText>
+                        <h2>Wing Landing Gear Shock Struct Fluid Replacement</h2>
+                      </IonText>
+                    </IonCol>
+                    <IonCol>
+                      <IonCardContent class="ion-no-padding">
+                        <IonImg src={planeImg} />
+                        <IonText>9V-SPK BOEING-747-412</IonText>
+                      </IonCardContent>
+                    </IonCol>
+                  </IonRow>
+                      <IonText>Progress:</IonText>
+                      <IonProgressBar
+                        color="success"
+                        value={1}
+                      ></IonProgressBar>
+                </IonGrid>
+              </IonCard>
+            </IonList>
+          </IonContent>
+        </IonCardContent>
+      </IonCard>
+    );
+  }
+}
+
+const authCondition = (authUser: any) => !!authUser;
+
+export const TaskList = withAuthorization(authCondition)(TaskListComponent);
